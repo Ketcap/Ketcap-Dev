26bc7076b4ce6451b91c1c2316cdfd30d856e013
Uğur Oruç <uoruc5@hotmail.com>
2022-12-25 16:34:18 +0300

    add: about and ui library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,25 @@
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "dark",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
       <Analytics />
     </>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,31 +1,39 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { createGetInitialProps } from "@mantine/next";
 
-export default function Document() {
-  const meta = {
-    title: "Ketcap's Personal Notes",
-    description: 'Posts about programming, tech, and life.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png' // @TODO replace this
+const getInitialProps = createGetInitialProps();
+
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps;
+
+  render() {
+    const meta = {
+      title: "Ketcap's Personal Notes",
+      description: "Posts about programming, tech, and life.",
+      image:
+        "https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png", // @TODO replace this
+    };
+
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="robots" content="follow, index" />
+          <meta name="description" content={meta.description} />
+          <meta property="og:site_name" content={meta.title} />
+          <meta property="og:description" content={meta.description} />
+          <meta property="og:title" content={meta.title} />
+          <meta property="og:image" content={meta.image} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@uur_oruc" />
+          <meta name="twitter:title" content={meta.title} />
+          <meta name="twitter:description" content={meta.description} />
+          <meta name="twitter:image" content={meta.image} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
-
-  return (
-    <Html lang="en">
-      <Head>
-        <meta name="robots" content="follow, index" />
-        <meta name="description" content={meta.description} />
-        <meta property="og:site_name" content={meta.title} />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@uur_oruc" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
 }
