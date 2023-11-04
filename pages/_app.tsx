30a16e5330a83b95a4b97f36b33975ffbddc17ce
Uğur Oruç <uoruc5@hotmail.com>
2023-11-04 21:27:29 +0100

    add: preferedcolorscheme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import { Analytics } from "@vercel/analytics/react";
 import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
+import { useColorScheme } from "@mantine/hooks";
 
 function MyApp({ Component, pageProps }) {
+  const preferredColorScheme = useColorScheme();
   return (
     <>
       <Head>
@@ -11,7 +13,13 @@ function MyApp({ Component, pageProps }) {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
-      <MantineProvider withGlobalStyles withNormalizeCSS>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: preferredColorScheme,
+        }}
+      >
         <Component {...pageProps} />
       </MantineProvider>
       <Analytics />
